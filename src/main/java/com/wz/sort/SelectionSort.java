4c9ad4d64af587d92d2e1cc845fcefe231347866
wuzhuang <wuz0316@gmail.com>
2019-07-20 23:39:05 +0800

    '-'

--- src/main/java/com/wz/sort/SelectionSort.java
+++ src/main/java/com/wz/sort/SelectionSort.java
@@ -5,15 +5,29 @@ package com.wz.sort;
  * 选择排序
  */
 public class SelectionSort {
+    public static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
     public static void selectionSort(int[] arr) {
         if(arr == null || arr.length < 2) {
             return;
         }
-        int min = 0;
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                min = arr[j] > min ? arr[j] : min;
+            int min = i;
+            for (int j = i; j < arr.length - 1; j++) {
+                min = arr[j] < arr[min] ? j : min;
             }
+            swap(arr, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 7, 3, 0, 9, 5};
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 }
