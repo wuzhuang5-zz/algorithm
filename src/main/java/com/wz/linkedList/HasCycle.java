6c3743606f096e967538247fd66765e797ae6b87
wuzhuang5 <wuz0316@gmail.com>
2019-10-23 22:57:42 +0800

    -

--- src/main/java/com/wz/linkedList/HasCycle.java
+++ src/main/java/com/wz/linkedList/HasCycle.java
@@ -1,5 +1,10 @@
 package com.wz.linkedList;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Author: wz
  * @Date: 2019/10/22 10:28 下午
@@ -9,10 +14,25 @@ public class HasCycle {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-
+        while (slow != null && fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean hasCycleByHash(ListNode head) {
-
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
     }
 }
