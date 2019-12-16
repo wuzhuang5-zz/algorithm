6ff24a023f2ded12bbc332a31c98d8336124f822
wuzhuang5 <wuz0316@gmail.com>
2019-12-16 22:44:51 +0800

    al

--- src/main/java/com/wz/linkedList/AddTwoLinkedLists.java
+++ src/main/java/com/wz/linkedList/AddTwoLinkedLists.java
@@ -1,7 +1,5 @@
 package com.wz.linkedList;
 
-import org.w3c.dom.NodeList;
-
 /**
  * 两个单链表生成相加链表
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -9,7 +7,43 @@ import org.w3c.dom.NodeList;
  * 原因：342 + 465 = 807
  */
 public class AddTwoLinkedLists {
-    public ListNode addTwoLinkedList(NodeList l1, NodeList l2) {
-        return null;
+    public ListNode addTwoLinkedList(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.value : 0;
+            int y = (l2 != null) ? l2.value : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry != 0) {
+            curr.next = new ListNode(0);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode newList = new AddTwoLinkedLists().addTwoLinkedList(l1, l2);
+        while (newList != null) {
+            System.out.println(newList.value);
+            newList = newList.next;
+        }
     }
 }
