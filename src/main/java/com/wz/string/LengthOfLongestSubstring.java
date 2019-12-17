64c43b1e15de41578e982aa92708427c48b984d5
wuzhuang5 <wuz0316@gmail.com>
2019-12-17 19:29:54 +0800

    ai

--- src/main/java/com/wz/string/LengthOfLongestSubstring.java
+++ src/main/java/com/wz/string/LengthOfLongestSubstring.java
@@ -1,5 +1,8 @@
 package com.wz.string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 无重复字符的最长子串
  * 输入: "abcabcbb"
@@ -9,6 +12,26 @@ package com.wz.string;
  */
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() < 1) {
+            return -1;
+        }
+        char[] arr = s.toCharArray();
+        int result = 0;
+        Set<Character> set = new HashSet<>();
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(arr[i])) {
+                result = Math.max(result, set.size());
+                set.clear();
+            }
+            set.add(arr[i]);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        String s = "abcabcdbb";
+        int longLength = new LengthOfLongestSubstring().lengthOfLongestSubstring(s);
+        System.out.println(longLength);
     }
 }
