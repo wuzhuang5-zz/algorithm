bb624108829b1e39b818f63f864dd00b7962891d
wuzhuang5 <wuz0316@gmail.com>
2019-12-19 16:24:57 +0800

    byte dance

--- bytedance/src/main/java/com/bytedance/array/Trap.java
+++ bytedance/src/main/java/com/bytedance/array/Trap.java
@@ -9,7 +9,40 @@ package com.bytedance.array;
  * 输出: 6
  */
 public class Trap {
-    public int trap(int[] height) {
+    /**
+     * 按列查找  时间复杂度O(n*n) 空间复杂度O(n)
+     * @param height
+     * @return
+     */
+    public int trap1(int[] height) {
+        int sum = 0;
+        //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
+        for (int i = 1; i < height.length - 1; i++) {
+            int max_left = 0;
+            //找出左边最高
+            for (int j = i - 1; j >= 0; j--) {
+                if (height[j] > max_left) {
+                    max_left = height[j];
+                }
+            }
+            int max_right = 0;
+            //找出右边最高
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] > max_right) {
+                    max_right = height[j];
+                }
+            }
+            //找出两端较小的
+            int min = Math.min(max_left, max_right);
+            //只有较小的一段大于当前列的高度才会有水，其他情况不会有水
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+    public int trap2(int[] height) {
         return 0;
     }
 }
--- bytedance/src/main/java/com/bytedance/string/LongestPalindrome.java
+++ bytedance/src/main/java/com/bytedance/string/LongestPalindrome.java
@@ -10,14 +10,15 @@ package com.bytedance.string;
  * 注意: "aba" 也是一个有效答案。
  */
 public class LongestPalindrome {
+
     //暴力法 两层循环遍历
-    public String longestPalindrome(String s) {
+    public static String longestPalindrome(String s) {
         //存储最大回文子串
         String ans = "";
         int length = s.length();
         int max = 0;
         for (int i = 0; i < length; i++) {
-            for (int j = i+1; j <= length; j++) {
+            for (int j = i + 1; j <= length; j++) {
                 String tmp = s.substring(i, j);
                 if (isPalindromic(tmp) && tmp.length() > max) {
                     ans = tmp;
@@ -28,14 +29,9 @@ public class LongestPalindrome {
         return ans;
     }
 
-    /**
-     * 判断一个字符串是否是回文
-     * @param s
-     * @return
-     */
-    private boolean isPalindromic(String s) {
+    private static boolean isPalindromic(String s) {
         int length = s.length();
-        for (int i = 0; i < length/2 ; i++) {
+        for (int i = 0; i < length / 2; i++) {
             if (s.charAt(i) != s.charAt(length - i - 1)) {
                 return false;
             }
@@ -43,30 +39,66 @@ public class LongestPalindrome {
         return true;
     }
 
-    /**
-     * 动态规划   时间复杂度O(n*n)
-     * @param s
-     * @return
-     */
-    public String longestPalindrome2(String s) {
-        int n = s.length();
-        String res = "";
-        boolean[] P = new boolean[n];
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= i; j--) {
-                P[j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || P[j - 1]);
-                if (P[j] && j - i + 1 > res.length()) {
-                    res = s.substring(i, j + 1);
+
+    public static String longestPalindrome2(String s) {
+        int length = s.length();
+        boolean[][] P = new boolean[length][length];
+        int maxLen = 0;
+        String maxPal = "";
+        for (int i = 1; i <= length; i++) {
+            for (int j = 0; j < length; j++) {
+                int end = j + i - 1;
+                if (end >= length) {
+                    break;
+                }
+                P[j][end] = (i == 1 || i == 2 || P[j + 1][end - 1]) && s.charAt(j) == s.charAt(end);
+                if (P[j][end] && i > maxLen) {
+                    maxPal = s.substring(j, end + 1);
                 }
             }
         }
-        return res;
+        return maxPal;
+    }
+        public static String longestPalindrome3(String s){
+            int n = s.length();
+            String res = "";
+            boolean[] P = new boolean[n];
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= i; j--) {
+                    P[j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || P[j - 1]);
+                    if (P[j] && j - i + 1 > res.length()) {
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+            return res;
+        }
+
+    public static String longestPalindrome4(String s) {
+        int length = s.length();
+        if (length <= 1) {
+            return s;
+        }
+        int longestPalindrome = 1;
+        String longestPalindromeStr = s.substring(0,1);
+        boolean[][] dp = new boolean[length][length];
+        for (int r = 1; r < length; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > longestPalindrome) {
+                        longestPalindrome = r-l+1;
+                        longestPalindromeStr = s.substring(l, r + 1);
+                    }
+                }
+            }
+        }
+        return longestPalindromeStr;
+    }
+        public static void main (String[]args){
+            String s = "ba";
+            String result = longestPalindrome3(s);
+            System.out.println(result);
+        }
     }
 
-
-    public static void main(String[] args) {
-        String s = "babadcccc";
-        String result = new LongestPalindrome().longestPalindrome(s);
-        System.out.println(result);
-    }
-}
