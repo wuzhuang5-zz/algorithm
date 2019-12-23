953bbea2808796530dc4b3e9d816ff3ea4838d9a
wuzhuang5 <wuz0316@gmail.com>
2019-12-23 20:27:52 +0800

    ai

---
+++ bytedance/src/main/java/com/bytedance/dp/ClimbStairs.java
@@ -0,0 +1,26 @@
+package com.bytedance.dp;
+
+/**
+ * 爬楼梯
+ */
+public class ClimbStairs {
+    public static int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int result = climbStairs(n);
+        System.out.println(result);
+    }
+}
---
+++ bytedance/src/main/java/com/bytedance/dp/MaxSubArray.java
@@ -0,0 +1,27 @@
+package com.bytedance.dp;
+
+/**
+ * leetcode 53
+ * 最大子序和
+ */
+public class MaxSubArray {
+    public static int maxSubArray(int[] arr) {
+        int ans = arr[0];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (sum > 0) {
+                sum += arr[i];
+            } else {
+                sum = arr[i];
+            }
+            ans = Math.max(sum, ans);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubArray(arr);
+        System.out.println(result);
+    }
+}
