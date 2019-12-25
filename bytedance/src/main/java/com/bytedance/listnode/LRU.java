49ffe7dd7327e233c00bef1b173ca8fb6b7577ad
wuzhuang5 <wuz0316@gmail.com>
2019-12-25 14:35:56 +0800

    lru

---
+++ bytedance/src/main/java/com/bytedance/listnode/LRU.java
@@ -0,0 +1,20 @@
+package com.bytedance.listnode;
+
+/**
+ * LRU缓存机制
+ */
+public class LRU {
+
+    /**
+     * 双向链表
+     */
+    static class Node {
+        private int key, val;
+        public Node next, prev;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
