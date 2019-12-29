d6546e50089b83e8df9f6d223921dcb597f73c2f
wuzhuang5 <wuz0316@gmail.com>
2019-12-29 10:43:58 +0800

    最短编辑距离

--- dp/MinDistance.java
+++ dp/MinDistance.java
@@ -19,21 +19,22 @@ public class MinDistance {
             int m = word1.length(), n = word2.length();
             int[][] dp = new int[m + 1][n + 1];
             // base case
-            for (int i = 1; i <= m; i++)
+            for (int i = 1; i <= m; i++) {
                 dp[i][0] = i;
-            for (int j = 1; j <= n; j++)
+            }
+            for (int j = 1; j <= n; j++) {
                 dp[0][j] = j;
+            }
             // 自底向上求解
-            for (int i = 1; i <= m; i++)
-                for (int j = 1; j <= n; j++)
-                    if (word1.charAt(i-1) == word2.charAt(j-1))
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (word1.charAt(i-1) == word2.charAt(j-1)) {
                         dp[i][j] = dp[i - 1][j - 1];
-                    else
-                        dp[i][j] = min(
-                                dp[i - 1][j] + 1,
-                                dp[i][j - 1] + 1,
-                                dp[i-1][j-1] + 1
-                        );
+                    } else {
+                        dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
+                    }
+                }
+            }
             // 储存着整个 s1 和 s2 的最小编辑距离
             return dp[m][n];
         }
