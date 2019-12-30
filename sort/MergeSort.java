08009bb5bb912f97b81c4ac1ca1d4203e01e7362
wuzhuang5 <wuz0316@gmail.com>
2019-12-30 17:51:09 +0800

    归并排序

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -2,5 +2,46 @@
  * 归并排序 O(nlogn)
  */
 public class MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
+    }
 
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = l+((r-l)>>>1);
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r-l+1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m+1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] > arr[p2] ? arr[p2++] : arr[p1++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (int j = 0; j < help.length; j++) {
+            arr[l + j] = help[j];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 8, 4, 3, 5, 7, 6};
+        mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
