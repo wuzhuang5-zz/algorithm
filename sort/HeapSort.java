fc9758195e43048b0392c9179e83ae53cb6af47d
wuzhuang5 <wuz0316@gmail.com>
2020-01-02 17:59:51 +0800

    sort

--- sort/HeapSort.java
+++ sort/HeapSort.java
@@ -2,4 +2,33 @@
  * 堆排
  */
 public class HeapSort {
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return ;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int size = arr.length;
+        swap(arr, 0, --size);
+        while (size > 0) {
+            heap
+        }
+    }
+    //大顶堆
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index-1)/2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+    //堆化
+    public static void heapify(int[] arr, int index, int size) {
+        int left = index*2+1;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
