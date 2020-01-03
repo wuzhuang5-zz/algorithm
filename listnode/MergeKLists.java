f4b94b694e074006a3985613a9a28678a93c60a6
wuzhuang5 <wuz0316@gmail.com>
2020-01-03 14:54:37 +0800

    链表

---
+++ listnode/MergeKLists.java
@@ -0,0 +1,19 @@
+/**
+ * leetcode 23
+ * 合并K个排序链表
+ *
+ * 输入:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * 输出: 1->1->2->3->4->4->5->6
+ */
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        for (ListNode listNode : lists) {
+
+        }
+    }
+}
