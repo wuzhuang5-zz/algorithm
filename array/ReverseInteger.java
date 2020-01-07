efad151187ff24b635d79deb9701580ed01a8fe4
wuzhuang5 <wuz0316@gmail.com>
2020-01-07 12:40:45 +0800

    整数反转

---
+++ array/ReverseInteger.java
@@ -0,0 +1,21 @@
+/**
+ * leetcode 7
+ * 整数反转
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+}
--- dp/MaxProfit.java
+++ dp/MaxProfit.java
@@ -16,7 +16,8 @@ public class MaxProfit {
         if(prices.length <= 1) {
             return 0;
         }
-        int min = prices[0], max = 0;
+        int min = prices[0];
+        int max = 0;
         for(int i = 1; i < prices.length; i++) {
             max = Math.max(max, prices[i] - min);
             min = Math.min(min, prices[i]);
