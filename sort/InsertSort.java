626d7e9fe21bad830c56d38078eaf85206de1c11
wuzhuang5 <wuz0316@gmail.com>
2020-03-05 16:32:47 +0800

    排序

--- sort/InsertSort.java
+++ sort/InsertSort.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 /**
  * 插入排序
  */
 public class InsertSort {
+    public static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i-1; j >= 0 && nums[j] > nums[j+1] ; j--) {
+                swap(nums, j, j + 1);
+            }
+        }
+    }
 
+    private static void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 3, 7, 1, 10, 23};
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 }
+
--- sort/SelectSort.java
+++ sort/SelectSort.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 /**
  * 选择排序  时间复杂度O(n*n)
  */
 public class SelectSort {
+    public static void selectSort(int[] nums) {
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    swap(nums, i, j);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 3, 7, 1, 10, 23};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 }
