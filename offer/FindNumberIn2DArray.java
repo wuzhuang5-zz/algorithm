580e96a12630c19bb48a671f441f5af212718d5a
wuzhuang5 <wuz0316@gmail.com>
2020-03-18 20:47:08 +0800

    二维数组的查找

--- offer/FindNumberIn2DArray.java
+++ offer/FindNumberIn2DArray.java
@@ -2,7 +2,18 @@
  * 二维数组中的查找 面试题04
  * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
- *
+ *[
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
  */
 public class FindNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][0]
+        }
+    }
 }
