a216fb29b5018efe9395eff9018d7dcec9d9faec
wuzhuang5 <wuz0316@gmail.com>
2020-05-06 21:56:10 +0800

    二叉树

---
+++ tree/InvertTree.java
@@ -0,0 +1,23 @@
+import java.util.Queue;
+
+/**
+ * @Description: 翻转二叉树   226
+ * @Author: wz
+ */
+public class InvertTree {
+    //递归
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+        Queue
+    }
+}
